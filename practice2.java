479943927ca3b1481b2f8e0fd9aafa137819d550
Sruti Dash <sruti.dash1203@gmail.com>
2025-05-02 14:46:08 +0530

    infinite for loop due to lack of termination condition

---
+++ practice2.java
@@ -0,0 +1,8 @@
+public class practice2 {
+    public static void main(String[] args) {
+        for(; ;) { 
+            System.out.println("Apna College"); 
+        } 
+        //its an infinite loop, nothing has been initialied updated but there's no termination condition give that's why it becomes an infinite for loop
+    }
+}
