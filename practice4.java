3f0ab3daeb94fc70e71ffe757b725804ea15574a
Sruti Dash <sruti.dash1203@gmail.com>
2025-05-02 16:19:02 +0530

    for loop inifnite loop printing hello world

---
+++ practice4.java
@@ -0,0 +1,19 @@
+//Keep printing a message until the user stops it manually 
+
+import java.util.Scanner;
+
+public class practice4 {
+    public static void main(String[] args) {    
+        //for infinite loop  :
+        //if we want to enter someting to stop the loop
+        //we can use the same number which will be wrong
+        //or to print someting infinite times we can use for loop woth no termination condition like this :
+        //we can stop it manually by ctrl+c in terminal
+        for(; ;){
+            System.out.println("Hello, World!");
+        }
+    }
+}
+
+
+
